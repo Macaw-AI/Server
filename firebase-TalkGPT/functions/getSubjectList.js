d89583c9e11115ae6ea1fcf046a41e192b45a6ce
Mostiu <tomasz2k02@gmail.com>
2023-02-13 16:55:59 +0100

    added getSubjectList

---
+++ firebase-TalkGPT/functions/getSubjectList.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.getSubjectList = functions.https.onRequest(async (req, res) => {
+  const storage = admin.storage();
+  const options = {
+    prefix: `Scenarios/${req.body.data["language"]}/subjects/`,
+  };
+
+  const [files] = await storage.bucket().getFiles(options);
+  let promises = [];
+  for (const file of files) {
+    if(file.name.endsWith('.json')){
+        promises.push(file.download().then(data => {
+        return JSON.parse(data.toLocaleString());
+    }));
+    }
+    
+  }
+
+  const subjectList = await Promise.all(promises);
+  res.status(200).send({ listOfSubjects: subjectList });
+  
+});

505a05733d051a418a2635e544d46523dc143334
Wiktor <266597@student.pwr.edu.pl>
2023-02-28 15:42:57 +0100

    Added package-lock.json to .gitignore, changed recent conversation format in textToResponse.js

--- firebase-TalkGPT/functions/textToResponse.js
+++ firebase-TalkGPT/functions/textToResponse.js
@@ -9,9 +9,7 @@ function createPrompt(teacher, student, subject) {
     return `Response as my Teacher (${teacher.name},${teacher.character}). I am your Student(${student.name},${student.age}).
     During our conversation you should ask me about ${subject.name} and ${subject.questions} and let us debate about ${subject.related}.
     Remember to ask ONLY ONE question at the time and be curious obout my opinion and point of view.YOU start the conversation as a teacher. Below is a record of our recent quotes:
-    ${teacher.name}: Hi
-    Student: ${student.text}
-    ${teacher.name}: `;
+    ${student.text}`;
 }
 
 async function getBotResponse(prompt, teacher, student) {
